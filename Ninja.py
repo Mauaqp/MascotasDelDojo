8ce323bdbca46346bbf45351a86691afe16b4bb8
Mauaqp <peraltilla93@hotmail.com>
2022-01-19 13:06:00 -0500

    Avance con variables para seleccionar entre mascotas

--- Ninja.py
+++ Ninja.py
@@ -16,20 +16,18 @@ class Ninja:
         print(self.nombre)
         return self.nombre
     
-    def caminar(self):
-        print(self.nombre + " sacó a pasear a " + Mascota.lista_mascotas[0].name)
-        Mascota.lista_mascotas[0].jugar()
+    def caminar(self,x):
+        print(self.nombre + " sacó a pasear a " + Mascota.lista_mascotas[x].name)
+        Mascota.lista_mascotas[x].jugar()
         return self
 
-    def alimentar(self):
-        print(self.nombre + " alimentó a " + Mascota.lista_mascotas[0].name)
-        Mascota.lista_mascotas[0].comer()
+    def alimentar(self,x):
+        print(self.nombre + " alimentó a " + Mascota.lista_mascotas[x].name)
+        Mascota.lista_mascotas[x].comer()
         return self
     
-    def bañar(self):
-        print(self.nombre + " bañó a " + Mascota.lista_mascotas[0].name)
-        Mascota.lista_mascotas[0].sonido()
-        Mascota.lista_mascotas[0].dormir()
+    def bañar(self,x):
+        print(self.nombre + " bañó a " + Mascota.lista_mascotas[x].name)
+        Mascota.lista_mascotas[x].sonido()
+        Mascota.lista_mascotas[x].dormir()
         return self
-
-    
--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from Ninja import Ninja
 from Mascota import Mascota
 
 #Instrucciones
-ninja1 = Ninja("Mauri", "Peraltilla", "Milly", "Galletita", "Hueso")
+ninja1 = Ninja("Mauri", "Peraltilla", "Galleto", "Galletita", "Hueso")
 mascota1 = Mascota("Milly", "Perrito", "Galletitas ricas", "100", "50")
 mascota2 = Mascota("Brownie", "Perrito", "Galletitas de pollito", "100", "100")
 ninja1.getNombre()
-ninja1.caminar().alimentar().bañar()
+ninja1.caminar(0).alimentar(0).bañar(1)
